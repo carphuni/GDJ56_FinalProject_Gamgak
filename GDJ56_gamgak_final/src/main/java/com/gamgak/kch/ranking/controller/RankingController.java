e7123ddbb1c0cebabe3c8fe0e49f349c31791e86
chanhee <chanhee@DESKTOP-PS8QVL3>
2023-01-26 12:06:31 +0900

    202301261206_kch : 그냥커밋..

--- GDJ56_gamgak_final/src/main/java/com/gamgak/kch/ranking/controller/RankingController.java
+++ GDJ56_gamgak_final/src/main/java/com/gamgak/kch/ranking/controller/RankingController.java
@@ -13,29 +13,29 @@ import com.gamgak.kch.ranking.service.RankingService;
 
 @Controller
 public class RankingController {
-   
-   private RankingService service;
-   
-   @Autowired
-   public RankingController(RankingService service) {
-      this.service=service;
-   }
-   
-   @RequestMapping("/ranking")
-   public String ranking() {
-      return "kch_ranking/ranking";
-   }
-   
-   @RequestMapping("/map/searchRes")
-   @ResponseBody
-   public List<Map> searchPlace(
-         @RequestParam(value="swLat") double swLat,
-         @RequestParam(value="swLon") double swLon,
-         @RequestParam(value="neLat") double neLat,
-         @RequestParam(value="neLon") double neLon) {
-      List<Map> list=service.searchPlace(swLat,swLon,neLat,neLon);
-      return list;
+	
+	private RankingService service;
+	
+	@Autowired
+	public RankingController(RankingService service) {
+		this.service=service;
+	}
+	
+	@RequestMapping("/ranking")
+	public String ranking() {
+		return "kch_ranking/ranking";
+	}
+	
+	@RequestMapping("/map/searchRes")
+	@ResponseBody
+	public List<Map> searchPlace(
+			@RequestParam(value="swLat") double swLat,
+			@RequestParam(value="swLon") double swLon,
+			@RequestParam(value="neLat") double neLat,
+			@RequestParam(value="neLon") double neLon) {
+		List<Map> list=service.searchPlace(swLat,swLon,neLat,neLon);
+		return list;
 
-   
-   }
+	
+	}
 }
